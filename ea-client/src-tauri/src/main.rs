fe822a33ce2c816d7e1f02698d08dec19fbf865d
Somkid Chaihanid <somkidchaihanid@MacBook-Air-khxng-Somkid.local>
2026-04-06 21:16:22 +0800

    Fix: compiler warnings for unused variables in main.rs

--- ea-client/src-tauri/src/main.rs
+++ ea-client/src-tauri/src/main.rs
@@ -28,7 +28,7 @@ fn main() {
     tauri::Builder::default()
         .plugin(tauri_plugin_opener::init())
         .plugin(tauri_plugin_shell::init())
-        .plugin(tauri_plugin_single_instance::init(|app, args, cwd| {}))
+        .plugin(tauri_plugin_single_instance::init(|_app, _args, _cwd| {}))
         .plugin(tauri_plugin_updater::Builder::new().build())
         .invoke_handler(tauri::generate_handler![open_chrome_incognito])
         .run(tauri::generate_context!())
